bc7c469d08e3d2c6fc23ea8be935e79dbfb1dc97
Michael Villalba Sotelo <visomi.dev@gmail.com>
2023-10-07 04:57:48 -0600

    fix: bad class on textfield

--- src/client/app/components/ui/textfield.tsx
+++ src/client/app/components/ui/textfield.tsx
@@ -31,7 +31,7 @@ export const TextFieldInput: ParentComponent<
   return (
     <TextFieldPrimitive.Input
       class={cn(
-        'flex h-9 w-full rounded-md border border-slate-200 bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-400 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:border-slate-800 dark:bg-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-800',
+        'flex h-9 w-full rounded-md border border-slate-200 bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-400 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-800',
         local.class,
       )}
       {...rest}
